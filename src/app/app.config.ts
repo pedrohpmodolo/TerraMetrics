37cb783427bc054a505814c62f329d29b031e0d7
Pedro Modolo <modolo.ph@gmail.com>
2025-07-25 22:48:24 -0400

    feat: move firebase config to environment file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "terrametrics-12e6a", appId: "1:212953607226:web:bfd6ddb75615b7f6fbd903", storageBucket: "terrametrics-12e6a.firebasestorage.app", apiKey: "AIzaSyCa8wja3O5CY-behADtxs5dEEFQmftSeUo", authDomain: "terrametrics-12e6a.firebaseapp.com", messagingSenderId: "212953607226", measurementId: "G-GMR75EQ01K" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
-    provideHttpClient()
+    provideRouter(routes),
+    provideHttpClient(),
+    
+    // Initialize Firebase with the configuration from environment
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
